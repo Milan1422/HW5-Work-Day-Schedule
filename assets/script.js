534193d14b0affb20d9aa3ecd824dc1c990778c0
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-01-22 23:07:34 -0500

    wrorking on saving task to the local storage

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
 
+// array of buttons
+let buttonArray = [];
+
 // Save input to the time block
 $("#btn7").click(function(){
-    console.log("clicked");
     let newListItem = $("#description7").val();
     if (newListItem.length > 0){
-        $("#description7").append("<li>" + newListItem + "</li>");
-        $("#description7").val();
+        // $("#description7").val("");
+        buttonArray.push({
+            time: "7am",
+            description: newListItem
+        })
+        localStorage.setItem("new added task", JSON.stringify(buttonArray));
         console.log(newListItem);
+
+        
     }
+
+
+});
+
+$(":button").on("click", function(){    
+    let newTask = $(this).attr("data-time");
+    console.log(newTask);
+    let textTask = $(".description").val();
+    $(".description").change(function(event){
+        $("input").append("<p>" + textTask + "</p>")
+        // console.log(textTask)
+    })
+
+
+
+    });
 });
 
 
 
-});
+
+
+
+
