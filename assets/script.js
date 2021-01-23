a55eb8cd96491f320690586a3ff91c98533c4afa
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-01-23 14:07:18 -0500

    changed my timeblick format

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,212 @@
 $(document).ready(function(){
 
-// array of buttons
-let buttonArray = [];
+    // array of buttons
+    let buttonArray = [];
 
-// Save input to the time block
-$("#btn7").click(function(){
-    let newListItem = $("#description7").val();
-    if (newListItem.length > 0){
-        // $("#description7").val("");
-        buttonArray.push({
-            time: "7am",
-            description: newListItem
-        })
-        localStorage.setItem("new added task", JSON.stringify(buttonArray));
-        console.log(newListItem);
+    // Save input to the time block
+    $("#btn7").click(function(){
+        let newListItem = $("#description7").val();
+        if (newListItem.length > 0){
+            // $("#description7").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
 
-        
-    }
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
 
-
-});
-
-$(":button").on("click", function(){    
-    let newTask = $(this).attr("data-time");
-    console.log(newTask);
-    let textTask = $(".description").val();
-    $(".description").change(function(event){
-        $("input").append("<p>" + textTask + "</p>")
-        // console.log(textTask)
-    })
-
-
+            newListItem.innerHtml += `${value}`
+            
+        }
 
     });
+    $("#btn8").click(function(){
+        let newListItem = $("#description8").val();
+        if (newListItem.length > 0){
+            // $("#description8").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+
+    $("#btn9").click(function(){
+        let newListItem = $("#description9").val();
+        if (newListItem.length > 0){
+            // $("#description9").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn10").click(function(){
+        let newListItem = $("#description10").val();
+        if (newListItem.length > 0){
+            // $("#description10").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn11").click(function(){
+        let newListItem = $("#description11").val();
+        if (newListItem.length > 0){
+            // $("#description11").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn12").click(function(){
+        let newListItem = $("#description12").val();
+        if (newListItem.length > 0){
+            // $("#description12").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn13").click(function(){
+        let newListItem = $("#description13").val();
+        if (newListItem.length > 0){
+            // $("#description13").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn14").click(function(){
+        let newListItem = $("#description14").val();
+        if (newListItem.length > 0){
+            // $("#description14").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn15").click(function(){
+        let newListItem = $("#description15").val();
+        if (newListItem.length > 0){
+            // $("#description15").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    $("#btn16").click(function(){
+        let newListItem = $("#description16").val();
+        if (newListItem.length > 0){
+            // $("#description16").val("");
+            buttonArray.push({
+                description: newListItem
+            })
+            localStorage.setItem("new added task", newListItem);
+            console.log(newListItem);
+        }
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+
+            newListItem.innerHtml += `${value}`
+            
+        }
+
+    });
+    
+    
 });
 
 
